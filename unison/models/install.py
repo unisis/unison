c8e3b61dd7d31adc3226f753fe0e8ce2ab2b00bd
unisisdev <contacto@unisis.com.ar>
2016-10-13 08:36:09 -0300

    Added install statuses

--- unison/models/install.py
+++ unison/models/install.py
@@ -10,6 +10,20 @@ class Install(models.Model):
     _name = 'unison.install'
     _order = 'name'
 
+    statuses = [
+        ('inserted', 'Inserted'),         # This is the first status, inserted in database, creation not sent to DigitalOcean yet
+        ('provisioning', 'Provisioning'), # Creation of node sent to DigitalOcean, waiting their creation
+        ('installing', 'Installing'),     # Node was created, we are initializing/setting their containers
+        ('starting', 'Starting'),         # Initialization finalized, requested start of services (also used after stop an installation)
+        ('started', 'Started'),           # Installation is online and ready to be used
+        ('stopping', 'Stopping'),         # UniSon or user has requested the stop of the installation
+        ('stopped', 'Stopped'),           # Installation was stopped intentionally
+        ('crashed', 'Crashed'),           # Installation should be online/started, but it seems unresponsive/crashed at this moment
+        ('deleted', 'Deleted'),           # Installation was deleted from database, termination not sent to DigitalOcean yet
+        ('terminating', 'Terminating'),   # Requested termination of node on DigitalOcean
+        ('terminated', 'Terminated'),     # Node was termnated as well any other resource related to this installation
+    ]
+
     name = fields.Char('Name', required=True, index=True)
     description = fields.Char('Description', required=True, index=True)
     distro_id = fields.Many2one('unison.distro', 'Distro', ondelete='restrict')
@@ -17,7 +31,7 @@ class Install(models.Model):
     partner_id = fields.Many2one('res.partner', 'Client', ondelete='restrict')
     manage_dns = fields.Boolean('Manage DNS', default=True)
     site_record_id = fields.Many2one('unison.record', 'DNS Record', ondelete='restrict')
-    status = fields.Char('Status')
+    status = fields.Selection(statuses, 'Status', default='inserted')
     admin_pwd = fields.Char('Admin Password')
     prod_distro_version_id = fields.Many2one('unison.version', 'Prod Distro Version', required=True, ondelete='restrict')
     prod_install_version_id = fields.Many2one('unison.version', 'Prod Install Version', ondelete='restrict')
